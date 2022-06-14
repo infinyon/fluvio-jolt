c4341cb881ad2beb7231ef15da36841cc58464ea
Alexander Galibey <alexander.galibey@infinyon.com>
2022-06-14 16:15:26 +0300

    Add basic Shift operation

--- src/shift.rs
+++ src/shift.rs
@@ -1,6 +1,125 @@
-use serde_json::Value;
+use serde_json::{Map, Value};
+use serde_json::map::Entry;
 use crate::spec::Spec;
 
-pub(crate) fn shift(input: Value, spec: &Spec) -> Value {
-    unimplemented!("{:?} {:?}", input, spec)
+pub(crate) fn shift(mut input: Value, spec: &Spec) -> Value {
+    let mut result = Value::Object(Map::new());
+    for (path, leaf) in spec.iter() {
+        let new_position = match leaf {
+            Value::String(val) => val,
+            _ => continue,
+        };
+        if let Some(input_leaf) = input.pointer_mut(&path) {
+            let _ = insert(&mut result, new_position, input_leaf.take());
+        }
+    }
+    result
+}
+
+fn insert(dest: &mut Value, position: &str, val: Value) -> Option<()> {
+    let mut paths = position.split('.').collect::<Vec<&str>>();
+    paths.insert(0, "");
+    for i in 0..paths.len() - 1 {
+        let ancestor = dest.pointer_mut(paths[0..=i].join("/").as_str())?;
+        let next_name = paths[i + 1];
+        let next_object = if i < paths.len() - 2 {
+            Value::Object(Map::new())
+        } else {
+            Value::Null
+        };
+        match ancestor {
+            Value::Object(ref mut map) => {
+                if let Entry::Vacant(entry) = map.entry(next_name) {
+                    entry.insert(next_object);
+                }
+            }
+            other => {
+                let mut map = Map::new();
+                map.insert(next_name.to_string(), next_object);
+                *other = Value::Object(map);
+            }
+        };
+    }
+    let pointer_mut = dest.pointer_mut(paths.join("/").as_str())?;
+    *pointer_mut = val;
+    Some(())
+}
+
+#[cfg(test)]
+mod test {
+
+    use serde_json::json;
+    use super::*;
+
+    #[test]
+    fn test_insert_object_to_empty() {
+        //given
+        let mut empty_dest = Value::Null;
+        let value = json!({
+            "a": "b",
+        });
+
+        let result = insert(&mut empty_dest, "new", value);
+
+        assert!(result.is_some());
+        assert_eq!(
+            empty_dest,
+            json!({
+                "new": {
+                    "a": "b"
+                }
+            })
+        );
+    }
+
+    #[test]
+    fn test_insert_object_to_non_empty() {
+        //given
+        let mut empty_dest = json!({
+            "b": "bb",
+            "c": "cc",
+        });
+        let value = json!({
+            "a": "b",
+        });
+
+        let result = insert(&mut empty_dest, "new", value);
+
+        assert!(result.is_some());
+        assert_eq!(
+            empty_dest,
+            json!({
+                "b": "bb",
+                "c": "cc",
+                "new": {
+                    "a": "b"
+                }
+            })
+        );
+    }
+
+    #[test]
+    fn test_insert_object_to_empty_non_root() {
+        //given
+        let mut empty_dest = Value::Null;
+        let value = json!({
+            "a": "b",
+        });
+
+        let result = insert(&mut empty_dest, "level1.level2.new", value);
+
+        assert!(result.is_some());
+        assert_eq!(
+            empty_dest,
+            json!({
+                "level1": {
+                    "level2": {
+                        "new": {
+                            "a": "b"
+                        }
+                    }
+                }
+            })
+        );
+    }
 }
--- src/spec.rs
+++ src/spec.rs
@@ -20,6 +20,11 @@ pub(crate) enum Operation {
 #[derive(Debug, Serialize, Deserialize, Clone, Eq, PartialEq)]
 pub(crate) struct Spec(Value);
 
+#[derive(Debug)]
+pub(crate) struct SpecIter<'a> {
+    path: Vec<(&'a Value, usize, String)>,
+}
+
 impl TransformSpec {
     pub(crate) fn shift(spec: Value) -> Self {
         Self(vec![SpecEntry {
@@ -30,6 +35,50 @@ impl TransformSpec {
 
     pub(crate) fn entries(&self) -> impl Iterator<Item = &SpecEntry> {
         self.0.iter()
+    }
+}
+
+impl Spec {
+    pub(crate) fn iter(&self) -> SpecIter {
+        SpecIter::new(self)
+    }
+}
+
+impl<'a> SpecIter<'a> {
+    fn new(spec: &'a Spec) -> Self {
+        Self {
+            path: vec![(&spec.0, 0, String::new())],
+        }
+    }
+}
+
+impl<'a> Iterator for SpecIter<'a> {
+    type Item = (String, &'a Value);
+
+    fn next(&mut self) -> Option<Self::Item> {
+        loop {
+            let (current, index, name) = self.path.pop()?;
+            match current {
+                Value::Array(vec) => {
+                    if let Some(next) = vec.get(index) {
+                        self.path.push((current, index + 1, name));
+                        self.path.push((next, 0, index.to_string()));
+                    }
+                }
+                Value::Object(map) => {
+                    if let Some((next_name, next)) = map.iter().nth(index) {
+                        self.path.push((current, index + 1, name));
+                        self.path.push((next, 0, next_name.clone()));
+                    }
+                }
+                other => {
+                    let mut path: Vec<&str> =
+                        self.path.iter().map(|(_, _, path)| path.as_str()).collect();
+                    path.push(name.as_str());
+                    return Some((path.join("/"), other));
+                }
+            };
+        }
     }
 }
 
@@ -64,6 +113,47 @@ mod test {
                     "account": "__data.account"
                 }))
             }])
+        );
+    }
+
+    #[test]
+    fn test_spec_iter() {
+        let spec = r#"
+        [
+            {
+                "operation": "shift",
+                "spec": {
+                    "id": "__data.id",
+                    "name": "__data.name",
+                    "account": "__data.account",
+                    "address" : {
+                        "country": "ext.country",
+                        "city": "ext.city",
+                        "phones": ["12345","00000"]
+                    }
+                }
+            }
+        ]"#;
+        let result: TransformSpec = serde_json::from_str(spec).expect("parsed transform spec");
+
+        let spec_entry = result.entries().next().expect("one spec entry");
+
+        let items_vec = spec_entry
+            .spec
+            .iter()
+            .map(|(path, item)| format!("{}:{}", path, item))
+            .collect::<Vec<String>>();
+        assert_eq!(
+            items_vec,
+            vec![
+                "/account:\"__data.account\"",
+                "/address/city:\"ext.city\"",
+                "/address/country:\"ext.country\"",
+                "/address/phones/0:\"12345\"",
+                "/address/phones/1:\"00000\"",
+                "/id:\"__data.id\"",
+                "/name:\"__data.name\"",
+            ]
         );
     }
 }
