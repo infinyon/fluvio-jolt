b311eabaec6992b907f90a3c7dc54fad680ee653
Ozgur Akkurt <oezgurmakkurt@gmail.com>
2023-04-20 22:24:22 +0300

    save

--- tests/java/cardinality.rs
+++ tests/java/cardinality.rs
@@ -1,10 +1,6 @@
-use super::util::iter_json;
-use std::path::Path;
-use serde_json::Value as JsonValue;
+use super::util::test_dir;
 
 #[test]
 fn test_cardinality_transform() {
-    for (path, json) in iter_json(Path::new("tests/java/resources/cardinality")) {
-        todo!()
-    }
+    test_dir("tests/java/resources/cardinality", "cardinality");
 }
--- tests/java/shift.rs
+++ tests/java/shift.rs
@@ -1,1 +1,6 @@
+use super::util::test_dir;
 
+#[test]
+fn test_shift_transform() {
+    test_dir("tests/java/resources/shift", "shift");
+}
