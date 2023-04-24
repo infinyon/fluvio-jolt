69b0aeb9ed792f87faf04042d63cc100fcbe825b
Ozgur Akkurt <oezgurmakkurt@gmail.com>
2023-04-24 22:46:13 +0300

    add LhsTestCase

--- src/expr/parser.rs
+++ src/expr/parser.rs
@@ -13,13 +13,39 @@ mod tests {
     use super::*;
     use crate::expr::ast::Lhs;
 
+    struct LhsTestCase<'a> {
+        expr: &'a str,
+        expected: Vec<Lhs>,
+    }
+
+    impl<'a> LhsTestCase<'a> {
+        pub fn run(&self) {
+            let output = match LhsPipeParser::new().parse(self.expr) {
+                Ok(output) => output,
+                Err(e) => panic!("failed to parse {} as lhs\n{}", self.expr, e),
+            };
+
+            if output != self.expected {
+                panic!(
+                    "when parsing lhs.\nexpected={:#?}\ngot={:#?}",
+                    self.expected, output
+                );
+            }
+        }
+    }
+
     #[test]
     fn test_parse_lhs() {
-        let lhs = LhsPipeParser::new().parse("qwe*|*qwe*").unwrap();
+        LhsTestCase {
+            expr: "qwe*|*qwe*",
+            expected: vec![Lhs::RightStar("qwe".into()), Lhs::BothStar("qwe".into())],
+        }
+        .run();
 
-        assert_eq!(
-            vec![Lhs::RightStar("qwe".into()), Lhs::BothStar("qwe".into()),],
-            lhs
-        );
+        LhsTestCase {
+            expr: "&(0,1)",
+            expected: vec![Lhs::Amp(0, 1)],
+        }
+        .run();
     }
 }
