d28e951ba5db203ec4b5154136c53daa809ed771
Ozgur Akkurt <oezgurmakkurt@gmail.com>
2023-04-24 22:55:10 +0300

    add more tests

--- src/expr/parser.rs
+++ src/expr/parser.rs
@@ -9,7 +9,7 @@ lalrpop_mod!(
 pub use grammar::*;
 
 #[cfg(test)]
-mod tests {
+mod lhs_tests {
     use super::*;
     use crate::expr::ast::Lhs;
 
@@ -35,13 +35,83 @@ mod tests {
     }
 
     #[test]
-    fn test_parse_lhs() {
+    fn test_parse_lhs_square() {
         LhsTestCase {
-            expr: "qwe*|*qwe*",
-            expected: vec![Lhs::RightStar("qwe".into()), Lhs::BothStar("qwe".into())],
+            expr: "#my123_123key",
+            expected: vec![Lhs::Square("my123_123key".into())],
         }
         .run();
+    }
 
+    #[test]
+    fn test_parse_lhs_dollar_sign() {
+        LhsTestCase {
+            expr: "$",
+            expected: vec![Lhs::DollarSign],
+        }
+        .run();
+    }
+
+    #[test]
+    fn test_parse_lhs_key() {
+        LhsTestCase {
+            expr: "123my_key123",
+            expected: vec![Lhs::Key("123my_key123".into())],
+        }
+        .run();
+    }
+
+    #[test]
+    fn test_parse_lhs_star() {
+        LhsTestCase {
+            expr: "*",
+            expected: vec![Lhs::Star],
+        }
+        .run();
+    }
+
+    #[test]
+    fn test_parse_lhs_star_pipe() {
+        LhsTestCase {
+            expr: "qwe*|*qwe*|*qwe",
+            expected: vec![
+                Lhs::RightStar("qwe".into()),
+                Lhs::BothStar("qwe".into()),
+                Lhs::LeftStar("qwe".into()),
+            ],
+        }
+        .run();
+    }
+
+    #[test]
+    fn test_parse_lhs_at_full() {
+        LhsTestCase {
+            expr: "@(0,qwe)",
+            expected: vec![Lhs::At(Some((0, "qwe".into())))],
+        }
+        .run();
+    }
+
+    #[test]
+    fn test_parse_lhs_at_short() {
+        LhsTestCase {
+            expr: "@",
+            expected: vec![Lhs::At(None)],
+        }
+        .run();
+    }
+
+    #[test]
+    fn test_parse_lhs_amp_short() {
+        LhsTestCase {
+            expr: "&",
+            expected: vec![Lhs::Amp(0, 0)],
+        }
+        .run();
+    }
+
+    #[test]
+    fn test_parse_lhs_amp_full() {
         LhsTestCase {
             expr: "&(0,1)",
             expected: vec![Lhs::Amp(0, 1)],
