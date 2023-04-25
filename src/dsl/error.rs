9d4fedb877e931a2a3216adc7733cfb8af0c2a10
Ozgur Akkurt <oezgurmakkurt@gmail.com>
2023-04-26 02:50:54 +0300

    fix error and test compilation

--- src/dsl/error.rs
+++ src/dsl/error.rs
@@ -1,11 +1,29 @@
+use std::fmt;
+use std::error::Error;
+use thiserror::Error as ThisError;
+
 #[derive(Debug)]
 pub struct ParseError {
     pub(crate) pos: usize,
     pub(crate) cause: Box<ParseErrorCause>,
 }
 
-#[derive(Debug)]
+#[derive(Debug, ThisError)]
 pub enum ParseErrorCause {
+    #[error("Unexpected end of input.")]
     UnexpectedEndOfInput,
+    #[error("Unexpected character: '{0}'.")]
     UnexpectedChar(char),
 }
+
+impl fmt::Display for ParseError {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        write!(
+            f,
+            "Failed to parse input. Error near {}.\n{}",
+            self.pos, self.cause
+        )
+    }
+}
+
+impl Error for ParseError {}
